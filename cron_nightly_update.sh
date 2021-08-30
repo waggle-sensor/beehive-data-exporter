7914fdbf91058d1d3fb07b25a27905090b2e10bc
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-08-30 12:51:04 -0500

    instrumented job with last success and duration metric for prometheus

--- cron_nightly_update.sh
+++ cron_nightly_update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 cd $(dirname $0)
 
@@ -14,7 +14,22 @@ time ./export_data_chunks.py -m 'iio.*|env.*' "$start" "$end"
 echo "compiling bundle"
 time ./compile_bundle.py
 
-# upload to lcrc
+# upload to lcrc at:
+# https://web.lcrc.anl.gov/public/waggle/sagedata/SAGE-Data.tar
 echo "uploading bundle to lcrc"
 # NOTE this config is hardcoded for sage+lcrc. we can generalize this later.
 time rsync -av --remove-source-files --stats SAGE-Data.tar svcwagglersync:/home/svcwagglersync/waggle/public_html/sagedata/SAGE-Data.tar
+
+# generate and upload prometheus job metrics to lcrc at:
+# https://web.lcrc.anl.gov/public/waggle/sagedata/metrics.prom
+echo "uploading metrics to lcrc"
+cat > metrics.prom <<EOF
+# HELP job_last_success_unixtime UNIX epoch time when job passed.
+# TYPE job_last_success_unixtime gauge
+job_last_success_unixtime{job="sage_data_bundle"} $(date -u +%s)
+# HELP job_duration_seconds Job run duration in seconds.
+# TYPE job_duration_seconds gauge
+job_duration_seconds{job="sage_data_bundle"} $SECONDS
+EOF
+
+time rsync -av --remove-source-files --stats metrics.prom svcwagglersync:/home/svcwagglersync/waggle/public_html/sagedata/metrics.prom
