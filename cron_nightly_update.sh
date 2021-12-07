abd34d59499dcd666f98475d0f96102f8e8c3f1e
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-12-07 15:36:52 -0600

    parameterizing with env vars

--- cron_nightly_update.sh
+++ cron_nightly_update.sh
@@ -1,6 +1,32 @@
 #!/bin/bash -e
 
+DATA_DIR="${DATA_DIR:-data}"
+UPLOAD_HOST="${UPLOAD_HOST:-bebop.lcrc.anl.gov}"
+UPLOAD_USER="${UPLOAD_USER:-svcwagglersync}"
+UPLOAD_KEY="${UPLOAD_KEY:-~/.ssh/lcrc}"
+UPLOAD_DIR="${UPLOAD_DIR:-/home/svcwagglersync/waggle/public_html/sagedata/}"
+
+upload_files() {
+    for filename in $*; do
+        rsync \
+            --verbose \
+            --archive \
+            --remove-source-files \
+            --stats \
+            -e "ssh -i ${UPLOAD_KEY} -o StrictHostKeyChecking=no" \
+            "${filename}" \
+            "${UPLOAD_USER}@${UPLOAD_ADDR}:${UPLOAD_DIR}/${filename}"
+    done
+}
+
 cd $(dirname $0)
+
+echo "starting job"
+echo "DATA_DIR=${DATA_DIR}"
+echo "UPLOAD_ADDR=${UPLOAD_ADDR}"
+echo "UPLOAD_USER=${UPLOAD_USER}"
+echo "UPLOAD_KEY=${UPLOAD_KEY}"
+echo "UPLOAD_DIR=${UPLOAD_DIR}"
 
 # ensure last few days of data chunks are exported
 start=$(date -d '4 days ago' +'%Y-%m-%d')
@@ -8,21 +34,22 @@ start=$(date -d '4 days ago' +'%Y-%m-%d')
 end=$(date -d '1 day ago' +'%Y-%m-%d')
 
 echo "exporting data"
-time ./export_data_chunks.py -m 'iio.*|env.*' "$start" "$end"
+time ./export_data_chunks.py --datadir="${DATA_DIR}" --exclude "^sys.*" "${start}" "${end}"
 
 # compile SAGE-Data.tar
 echo "compiling bundle"
-time ./compile_bundle.py
+time ./compile_bundle.py --datadir="${DATA_DIR}"
 
 # upload to lcrc at:
 # https://web.lcrc.anl.gov/public/waggle/sagedata/SAGE-Data.tar
-echo "uploading bundle to lcrc"
+echo "uploading bundle"
+
 # NOTE this config is hardcoded for sage+lcrc. we can generalize this later.
-time rsync -av --remove-source-files --stats SAGE-Data.tar svcwagglersync:/home/svcwagglersync/waggle/public_html/sagedata/SAGE-Data.tar
+time upload_file SAGE-Data.tar
 
 # generate and upload prometheus job metrics to lcrc at:
 # https://web.lcrc.anl.gov/public/waggle/sagedata/metrics.prom
-echo "uploading metrics to lcrc"
+echo "uploading metrics"
 cat > metrics.prom <<EOF
 # HELP job_last_success_unixtime UNIX epoch time when job passed.
 # TYPE job_last_success_unixtime gauge
@@ -32,4 +59,4 @@ job_last_success_unixtime{job="sage_data_bundle"} $(date -u +%s)
 job_duration_seconds{job="sage_data_bundle"} $SECONDS
 EOF
 
-time rsync -av --remove-source-files --stats metrics.prom svcwagglersync:/home/svcwagglersync/waggle/public_html/sagedata/metrics.prom
+time upload_file metrics.prom
